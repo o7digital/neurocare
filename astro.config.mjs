57a3bacd1e23928a236551f190e2a06aa9ed4c3c
o7digital <101292703+o7digital@users.noreply.github.com>
2026-08-01 18:07:30 -0600

    Add article pages and site content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://neurodiversa.mx',
   vite: {
     plugins: [tailwindcss()]
   }
